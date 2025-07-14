02dabe81fe2095e4afdab3b946fb9f093e288acd
Qiu233 <lopi258@outlook.com>
2025-07-14 20:58:20 +0800

    make library project

--- Binary.lean
+++ Binary.lean
@@ -1,3 +1,6 @@
 -- This module serves as the root of the `Binary` library.
 -- Import modules here that should be built as part of the library.
 import Binary.Basic
+import Binary.Get
+import Binary.Put
+import Binary.Deriving
--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,3 @@ package "binary" where
   version := v!"0.1.0"
 
 lean_lib Binary where
-
-@[default_target]
-lean_exe "binary" where
-  root := `Main
